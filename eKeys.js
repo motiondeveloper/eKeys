2ac1892a4a7f8173aae62dca0539c709d5f7852c
timhaywood <timhaywood@users.noreply.github.com>
2018-03-21 08:37:47 +1100

    Initial commit

---
+++ eKeys.js
@@ -0,0 +1,50 @@
+var spline = new KeySpline(.3, 0.0, .1, 1)
+
+var dur = 2;
+v1 = value;
+v2 = value+200;
+
+t1 = 0;
+t2 = 2;
+
+dv = v2-v1;
+dt = t2-t1;
+
+var timeInput = Math.min(1, time/dt);
+var prg = spline.get(timeInput);
+
+value + dv*prg
+
+function KeySpline (mX1, mY1, mX2, mY2) {
+
+  this.get = function(aX) {
+    if (mX1 == mY1 && mX2 == mY2) return aX; // linear
+    return CalcBezier(GetTForX(aX), mY1, mY2);
+  }
+
+  function A(aA1, aA2) { return 1.0 - 3.0 * aA2 + 3.0 * aA1; }
+  function B(aA1, aA2) { return 3.0 * aA2 - 6.0 * aA1; }
+  function C(aA1)      { return 3.0 * aA1; }
+
+  // Returns x(t) given t, x1, and x2, or y(t) given t, y1, and y2.
+  function CalcBezier(aT, aA1, aA2) {
+    return ((A(aA1, aA2)*aT + B(aA1, aA2))*aT + C(aA1))*aT;
+  }
+
+  // Returns dx/dt given t, x1, and x2, or dy/dt given t, y1, and y2.
+  function GetSlope(aT, aA1, aA2) {
+    return 3.0 * A(aA1, aA2)*aT*aT + 2.0 * B(aA1, aA2) * aT + C(aA1);
+  }
+
+  function GetTForX(aX) {
+    // Newton raphson iteration
+    var aGuessT = aX;
+    for (var i = 0; i < 4; ++i) {
+      var currentSlope = GetSlope(aGuessT, mX1, mX2);
+      if (currentSlope == 0.0) return aGuessT;
+      var currentX = CalcBezier(aGuessT, mX1, mX2) - aX;
+      aGuessT -= currentX / currentSlope;
+    }
+    return aGuessT;
+  }
+}
