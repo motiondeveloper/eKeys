501912152f0d12042695101c174ba9704a7d571b
Tim Haywood <tim@haywood.org>
2019-04-02 09:59:39 +1100

    Formatted code

--- eKeys.jsx
+++ eKeys.jsx
@@ -1,11 +1,18 @@
 {
-  "AnimGroup": function() {
+  function AnimGroup() {
     // Version 2.1.2
 
     this.keys = [];
 
     // Keyframe object constructor
-    function EKey(keyTime = 0, keyValue = -9999, easeIn = 33, easeOut = 33, velocityIn = 0, velocityOut = 0) {
+    function EKey(
+      keyTime = 0,
+      keyValue = -9999,
+      easeIn = 33,
+      easeOut = 33,
+      velocityIn = 0,
+      velocityOut = 0,
+    ) {
       this.time = keyTime;
       this.value = keyValue;
       this.easeIn = easeIn;
@@ -14,16 +21,16 @@
       this.velocityOut = velocityOut;
     }
 
-    this.add = function(
+    this.add = function addKeyframe(
       keyTime,
       value,
       easeIn,
       easeOut,
       velocityIn,
-      velocityOut
+      velocityOut,
     ) {
       this.keys.push(
-        new EKey(keyTime, value, easeIn, easeOut, velocityIn, velocityOut)
+        new EKey(keyTime, value, easeIn, easeOut, velocityIn, velocityOut),
       );
     };
 
@@ -42,21 +49,17 @@
     const kSplineTableSize = 11;
     const kSampleStepSize = 1.0 / (kSplineTableSize - 1.0);
 
-    const float32ArraySupported = typeof Float32Array === "function";
+    const float32ArraySupported = typeof Float32Array === 'function';
 
     const A = (aA1, aA2) => 1.0 - 3.0 * aA2 + 3.0 * aA1;
     const B = (aA1, aA2) => 3.0 * aA2 - 6.0 * aA1;
-    const C = (aA1) => 3.0 * aA1;
+    const C = aA1 => 3.0 * aA1;
 
     // Returns x(t) given t, x1, and x2, or y(t) given t, y1, and y2.
-    const calcBezier = (aT, aA1, aA2) => {
-      return ((A(aA1, aA2) * aT + B(aA1, aA2)) * aT + C(aA1)) * aT
-    };
+    const calcBezier = (aT, aA1, aA2) => ((A(aA1, aA2) * aT + B(aA1, aA2)) * aT + C(aA1)) * aT;
 
     // Returns dx/dt given t, x1, and x2, or dy/dt given t, y1, and y2.
-    const getSlope = (aT, aA1, aA2) => {
-      return 3.0 * A(aA1, aA2) * aT * aT + 2.0 * B(aA1, aA2) * aT + C(aA1)
-    };
+    const getSlope = (aT, aA1, aA2) => 3.0 * A(aA1, aA2) * aT * aT + 2.0 * B(aA1, aA2) * aT + C(aA1);
 
     const binarySubdivide = (aX, aA, aB, mX1, mX2) => {
       let currentX;
@@ -71,8 +74,8 @@
           aA = currentT;
         }
       } while (
-        Math.abs(currentX) > SUBDIVISION_PRECISION &&
-        ++i < SUBDIVISION_MAX_ITERATIONS
+        Math.abs(currentX) > SUBDIVISION_PRECISION
+        && ++i < SUBDIVISION_MAX_ITERATIONS
       );
       return currentT;
     };
@@ -92,8 +95,8 @@
     const LinearEasing = x => x;
 
     const bezier = (mX1, mY1, mX2, mY2) => {
-      if (!(0 <= mX1 && mX1 <= 1 && 0 <= mX2 && mX2 <= 1)) {
-        throw new Error("bezier x values must be in [0, 1] range");
+      if (!(mX1 >= 0 && mX1 <= 1 && mX2 >= 0 && mX2 <= 1)) {
+        throw new Error('bezier x values must be in [0, 1] range');
       }
 
       if (mX1 === mY1 && mX2 === mY2) {
@@ -101,7 +104,7 @@
       }
 
       // Precompute samples table
-      let sampleValues = float32ArraySupported
+      const sampleValues = float32ArraySupported
         ? new Float32Array(kSplineTableSize)
         : new Array(kSplineTableSize);
       for (let i = 0; i < kSplineTableSize; ++i) {
@@ -123,25 +126,24 @@
         --currentSample;
 
         // Interpolate to provide an initial guess for t
-        const dist =
-          (aX - sampleValues[currentSample]) /
-          (sampleValues[currentSample + 1] - sampleValues[currentSample]);
+        const dist = (aX - sampleValues[currentSample])
+          / (sampleValues[currentSample + 1] - sampleValues[currentSample]);
         const guessForT = intervalStart + dist * kSampleStepSize;
         const initialSlope = getSlope(guessForT, mX1, mX2);
-        
+
         if (initialSlope >= NEWTON_MIN_SLOPE) {
           return newtonRaphsonIterate(aX, guessForT, mX1, mX2);
-        } else if (initialSlope === 0.0) {
-          return guessForT;
-        } else {
-          return binarySubdivide(
-            aX,
-            intervalStart,
-            intervalStart + kSampleStepSize,
-            mX1,
-            mX2
-          );
         }
+        if (initialSlope === 0.0) {
+          return guessForT;
+        }
+        return binarySubdivide(
+          aX,
+          intervalStart,
+          intervalStart + kSampleStepSize,
+          mX1,
+          mX2,
+        );
       };
 
       const bezierEasing = (x) => {
@@ -157,59 +159,59 @@
       return bezierEasing;
     };
 
-    this.anim = function(time) {
+    this.anim = function animateBetweenKeys(time) {
       const lastKeyNum = this.keys.length - 1;
       const lastKey = this.keys[lastKeyNum];
 
       // Check if time is outside of all keys
       if (time <= this.keys[0].time) {
         return this.keys[0].value;
-      } else if (time >= lastKey.time) {
+      }
+      if (time >= lastKey.time) {
         return lastKey.value;
 
         // Otherwise animate between keys
-      } else {
-        const curKeyNum = 0;
-
-        // Set current key to most recent keyframe
-        while (curKeyNum < lastKeyNum && time >= this.keys[curKeyNum + 1].time) {
-          curKeyNum++;
-        }
-
-        const curKey = this.keys[curKeyNum];
-        const nextKey = this.keys[curKeyNum + 1];
-
-        // Create easing spline based on current and next key
-        const easeSpline = bezier(
-          curKey.easeOut / 100,
-          curKey.velocityOut / 100,
-          1 - nextKey.easeIn / 100,
-          1 - nextKey.velocityIn / 100
-        );
-
-        // Animation details
-        const v1 = curKey.value;
-        const v2 = nextKey.value;
-
-        const t1 = curKey.time;
-        const t2 = nextKey.time;
-
-        // Delta calculations
-        const deltaV = v2 - v1;
-        const deltaT = t2 - t1;
-
-        // Move animation to t1
-        const movedTime = Math.min(time - t1, 0);
-
-        // Map time to speed
-        const timeInput = Math.min(1, movedTime / deltaT);
-
-        // Get progress value according to spline
-        const prg = easeSpline(timeInput);
-
-        // Animate between values according to progress amount
-        return v1 + deltaV * prg;
       }
+      let curKeyNum = 0;
+
+      // Set current key to most recent keyframe
+      while (curKeyNum < lastKeyNum && time >= this.keys[curKeyNum + 1].time) {
+        curKeyNum++;
+      }
+
+      const curKey = this.keys[curKeyNum];
+      const nextKey = this.keys[curKeyNum + 1];
+
+      // Create easing spline based on current and next key
+      const easeSpline = bezier(
+        curKey.easeOut / 100,
+        curKey.velocityOut / 100,
+        1 - nextKey.easeIn / 100,
+        1 - nextKey.velocityIn / 100,
+      );
+
+      // Animation details
+      const v1 = curKey.value;
+      const v2 = nextKey.value;
+
+      const t1 = curKey.time;
+      const t2 = nextKey.time;
+
+      // Delta calculations
+      const deltaV = v2 - v1;
+      const deltaT = t2 - t1;
+
+      // Move animation to t1
+      const movedTime = Math.min(time - t1, 0);
+
+      // Map time to speed
+      const timeInput = Math.min(1, movedTime / deltaT);
+
+      // Get progress value according to spline
+      const prg = easeSpline(timeInput);
+
+      // Animate between values according to progress amount
+      return v1 + deltaV * prg;
     };
   }
 }
