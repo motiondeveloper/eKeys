8afabc218b32e65a9eef254186143b7603594e44
Tim Haywood <timhay@l-5cg6114k21.ws.sbs.com.au>
2019-05-23 13:16:54 +1000

    Refactor: move bezier related code inside fn

--- eKeys.jsx
+++ eKeys.jsx
@@ -84,69 +84,70 @@
       .map((key, index) => validateKeyframe(key, index))
       .sort((a, b) => a.keyTime - b.keyTime);
 
-    /**
-     * https://github.com/gre/bezier-easing
-     * BezierEasing - use bezier curve for transition easing function
-     * by Gaëtan Renaudeau 2014 - 2015 – MIT License
-     */
-
-    // These values are established by empiricism with tests (tradeoff: performance VS precision)
-    const NEWTON_ITERATIONS = 4;
-    const NEWTON_MIN_SLOPE = 0.001;
-    const SUBDIVISION_PRECISION = 0.0000001;
-    const SUBDIVISION_MAX_ITERATIONS = 10;
-
-    const kSplineTableSize = 11;
-    const kSampleStepSize = 1.0 / (kSplineTableSize - 1.0);
-
-    const float32ArraySupported = typeof Float32Array === 'function';
-
-    const A = (aA1, aA2) => 1.0 - 3.0 * aA2 + 3.0 * aA1;
-    const B = (aA1, aA2) => 3.0 * aA2 - 6.0 * aA1;
-    const C = aA1 => 3.0 * aA1;
-
-    // Returns x(t) given t, x1, and x2, or y(t) given t, y1, and y2.
-    const calcBezier = (aT, aA1, aA2) => ((A(aA1, aA2) * aT + B(aA1, aA2)) * aT + C(aA1)) * aT;
-
-    // Returns dx/dt given t, x1, and x2, or dy/dt given t, y1, and y2.
-    const getSlope = (aT, aA1, aA2) => 3.0 * A(aA1, aA2) * aT * aT + 2.0 * B(aA1, aA2) * aT + C(aA1);
-
-    const binarySubdivide = (aX, aA, aB, mX1, mX2) => {
-      let currentX;
-      let currentT;
-      let i = 0;
-      do {
-        currentT = aA + (aB - aA) / 2.0;
-        currentX = calcBezier(currentT, mX1, mX2) - aX;
-        if (currentX > 0.0) {
-          aB = currentT;
-        } else {
-          aA = currentT;
-        }
-      } while (
-        Math.abs(currentX) > SUBDIVISION_PRECISION
-        && ++i < SUBDIVISION_MAX_ITERATIONS
-      );
-      return currentT;
-    };
-
-    const newtonRaphsonIterate = (aX, aGuessT, mX1, mX2) => {
-      for (let i = 0; i < NEWTON_ITERATIONS; ++i) {
-        const currentSlope = getSlope(aGuessT, mX1, mX2);
-        if (currentSlope === 0.0) {
-          return aGuessT;
-        }
-        const currentX = calcBezier(aGuessT, mX1, mX2) - aX;
-        aGuessT -= currentX / currentSlope;
-      }
-      return aGuessT;
-    };
-
-    const LinearEasing = x => x;
-
     // Creates bezier curve and returns function
     // to calculate eased value
     const bezier = (mX1, mY1, mX2, mY2) => {
+
+      /**
+       * https://github.com/gre/bezier-easing
+       * BezierEasing - use bezier curve for transition easing function
+       * by Gaëtan Renaudeau 2014 - 2015 – MIT License
+     */
+
+      // These values are established by empiricism with tests (tradeoff: performance VS precision)
+      const NEWTON_ITERATIONS = 4;
+      const NEWTON_MIN_SLOPE = 0.001;
+      const SUBDIVISION_PRECISION = 0.0000001;
+      const SUBDIVISION_MAX_ITERATIONS = 10;
+
+      const kSplineTableSize = 11;
+      const kSampleStepSize = 1.0 / (kSplineTableSize - 1.0);
+
+      const float32ArraySupported = typeof Float32Array === 'function';
+
+      const A = (aA1, aA2) => 1.0 - 3.0 * aA2 + 3.0 * aA1;
+      const B = (aA1, aA2) => 3.0 * aA2 - 6.0 * aA1;
+      const C = aA1 => 3.0 * aA1;
+
+      // Returns x(t) given t, x1, and x2, or y(t) given t, y1, and y2.
+      const calcBezier = (aT, aA1, aA2) => ((A(aA1, aA2) * aT + B(aA1, aA2)) * aT + C(aA1)) * aT;
+
+      // Returns dx/dt given t, x1, and x2, or dy/dt given t, y1, and y2.
+      const getSlope = (aT, aA1, aA2) => 3.0 * A(aA1, aA2) * aT * aT + 2.0 * B(aA1, aA2) * aT + C(aA1);
+
+      const binarySubdivide = (aX, aA, aB, mX1, mX2) => {
+        let currentX;
+        let currentT;
+        let i = 0;
+        do {
+          currentT = aA + (aB - aA) / 2.0;
+          currentX = calcBezier(currentT, mX1, mX2) - aX;
+          if (currentX > 0.0) {
+            aB = currentT;
+          } else {
+            aA = currentT;
+          }
+        } while (
+          Math.abs(currentX) > SUBDIVISION_PRECISION
+          && ++i < SUBDIVISION_MAX_ITERATIONS
+        );
+        return currentT;
+      };
+
+      const newtonRaphsonIterate = (aX, aGuessT, mX1, mX2) => {
+        for (let i = 0; i < NEWTON_ITERATIONS; ++i) {
+          const currentSlope = getSlope(aGuessT, mX1, mX2);
+          if (currentSlope === 0.0) {
+            return aGuessT;
+          }
+          const currentX = calcBezier(aGuessT, mX1, mX2) - aX;
+          aGuessT -= currentX / currentSlope;
+        }
+        return aGuessT;
+      };
+
+      const LinearEasing = x => x;
+
       if (!(mX1 >= 0 && mX1 <= 1 && mX2 >= 0 && mX2 <= 1)) {
         throw new Error('bezier x values must be in [0, 1] range');
       }
@@ -212,7 +213,7 @@
     };
 
     // Returns the final animated value
-    const animateKeys = function(time = requiredArgumentError('time', '.anim inputs')) {
+    const animateBetweenKeys = function(time = requiredArgumentError('time', '.anim inputs')) {
       checkTypes([[time, 'number']]);
       const lastKeyNum = keys.length - 1;
       const lastKey = keys[lastKeyNum];
@@ -284,7 +285,7 @@
     }
 
     const publicAPI = {
-      anim: animateKeys,
+      anim: animateBetweenKeys,
     }
 
     return publicAPI;
