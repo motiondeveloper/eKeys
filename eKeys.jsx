c236b7732058c5d1880123c971a2fbda82f1b665
Tim Haywood <timhay@l-5cg6114khb.ws.sbs.com.au>
2019-05-24 15:54:27 +1000

    lint: eslint

--- eKeys.jsx
+++ eKeys.jsx
@@ -1,15 +1,14 @@
 function AnimGroup(inputKeyframes = []) {
   // More reliable version of standard js typeof
-  const getType = value =>
-    Object.prototype.toString
-      .call(value)
-      .replace(/^\[object |\]$/g, '')
-      .toLowerCase();
+  const getType = value => Object.prototype.toString
+    .call(value)
+    .replace(/^\[object |\]$/g, '')
+    .toLowerCase();
 
   // Error message template for an incorrect type
   const typeErrorMessage = (variableName, expectedType, receivedType) => {
     throw new TypeError(
-      `${variableName} must be of type ${expectedType}. Received ${receivedType}`
+      `${variableName} must be of type ${expectedType}. Received ${receivedType}`,
     );
   };
 
@@ -30,15 +29,15 @@ function AnimGroup(inputKeyframes = []) {
     return typeErrorMessage(
       'expected type',
       'string or array',
-      getType(expectedType)
+      getType(expectedType),
     );
   };
 
   // Loops through an array of the format [variable, 'expectedType']
   // and checks if each variable is of the expected type and
   // returns a TypeError if it's not
-  const checkTypes = checkingArray => {
-    checkingArray.map(check => {
+  const checkTypes = (checkingArray) => {
+    checkingArray.map((check) => {
       const argumentType = getType(check[0]);
       const expectedType = check[1];
       if (!isValidType(argumentType, expectedType)) {
@@ -88,6 +87,60 @@ function AnimGroup(inputKeyframes = []) {
     .map((key, index) => validateKeyframe(key, index))
     .sort((a, b) => a.keyTime - b.keyTime);
 
+  // These values are established by empiricism with tests (tradeoff: performance VS precision)
+  const NEWTON_ITERATIONS = 4;
+  const NEWTON_MIN_SLOPE = 0.001;
+  const SUBDIVISION_PRECISION = 0.0000001;
+  const SUBDIVISION_MAX_ITERATIONS = 10;
+
+  const kSplineTableSize = 11;
+  const kSampleStepSize = 1.0 / (kSplineTableSize - 1.0);
+
+  const float32ArraySupported = typeof Float32Array === 'function';
+
+  const A = (aA1, aA2) => 1.0 - 3.0 * aA2 + 3.0 * aA1;
+  const B = (aA1, aA2) => 3.0 * aA2 - 6.0 * aA1;
+  const C = aA1 => 3.0 * aA1;
+
+  // Returns x(t) given t, x1, and x2, or y(t) given t, y1, and y2.
+  const calcBezier = (aT, aA1, aA2) => ((A(aA1, aA2) * aT + B(aA1, aA2)) * aT + C(aA1)) * aT;
+
+  // Returns dx/dt given t, x1, and x2, or dy/dt given t, y1, and y2.
+  const getSlope = (aT, aA1, aA2) => 3.0 * A(aA1, aA2) * aT * aT + 2.0 * B(aA1, aA2) * aT + C(aA1);
+
+  const binarySubdivide = (aX, aA, aB, mX1, mX2) => {
+    let currentX;
+    let currentT;
+    let i = 0;
+    do {
+      currentT = aA + (aB - aA) / 2.0;
+      currentX = calcBezier(currentT, mX1, mX2) - aX;
+      if (currentX > 0.0) {
+        aB = currentT;
+      } else {
+        aA = currentT;
+      }
+    } while (
+      Math.abs(currentX) > SUBDIVISION_PRECISION
+      && ++i < SUBDIVISION_MAX_ITERATIONS
+    );
+    return currentT;
+  };
+
+  const newtonRaphsonIterate = (aX, aGuessT, mX1, mX2) => {
+    for (let i = 0; i < NEWTON_ITERATIONS; ++i) {
+      const currentSlope = getSlope(aGuessT, mX1, mX2);
+      if (currentSlope === 0.0) {
+        return aGuessT;
+      }
+      const currentX = calcBezier(aGuessT, mX1, mX2) - aX;
+      aGuessT -= currentX / currentSlope;
+    }
+    return aGuessT;
+  };
+
+  const LinearEasing = x => x;
+
   // Creates bezier curve and returns function
   // to calculate eased value
   const bezier = (mX1, mY1, mX2, mY2) => {
@@ -96,62 +149,6 @@ function AnimGroup(inputKeyframes = []) {
      * BezierEasing - use bezier curve for transition easing function
      * by Gaëtan Renaudeau 2014 - 2015 – MIT License
      */
-
-    // These values are established by empiricism with tests (tradeoff: performance VS precision)
-    const NEWTON_ITERATIONS = 4;
-    const NEWTON_MIN_SLOPE = 0.001;
-    const SUBDIVISION_PRECISION = 0.0000001;
-    const SUBDIVISION_MAX_ITERATIONS = 10;
-
-    const kSplineTableSize = 11;
-    const kSampleStepSize = 1.0 / (kSplineTableSize - 1.0);
-
-    const float32ArraySupported = typeof Float32Array === 'function';
-
-    const A = (aA1, aA2) => 1.0 - 3.0 * aA2 + 3.0 * aA1;
-    const B = (aA1, aA2) => 3.0 * aA2 - 6.0 * aA1;
-    const C = aA1 => 3.0 * aA1;
-
-    // Returns x(t) given t, x1, and x2, or y(t) given t, y1, and y2.
-    const calcBezier = (aT, aA1, aA2) =>
-      ((A(aA1, aA2) * aT + B(aA1, aA2)) * aT + C(aA1)) * aT;
-
-    // Returns dx/dt given t, x1, and x2, or dy/dt given t, y1, and y2.
-    const getSlope = (aT, aA1, aA2) =>
-      3.0 * A(aA1, aA2) * aT * aT + 2.0 * B(aA1, aA2) * aT + C(aA1);
-
-    const binarySubdivide = (aX, aA, aB, mX1, mX2) => {
-      let currentX;
-      let currentT;
-      let i = 0;
-      do {
-        currentT = aA + (aB - aA) / 2.0;
-        currentX = calcBezier(currentT, mX1, mX2) - aX;
-        if (currentX > 0.0) {
-          aB = currentT;
-        } else {
-          aA = currentT;
-        }
-      } while (
-        Math.abs(currentX) > SUBDIVISION_PRECISION &&
-        ++i < SUBDIVISION_MAX_ITERATIONS
-      );
-      return currentT;
-    };
-
-    const newtonRaphsonIterate = (aX, aGuessT, mX1, mX2) => {
-      for (let i = 0; i < NEWTON_ITERATIONS; ++i) {
-        const currentSlope = getSlope(aGuessT, mX1, mX2);
-        if (currentSlope === 0.0) {
-          return aGuessT;
-        }
-        const currentX = calcBezier(aGuessT, mX1, mX2) - aX;
-        aGuessT -= currentX / currentSlope;
-      }
-      return aGuessT;
-    };
-
-    const LinearEasing = x => x;
 
     if (!(mX1 >= 0 && mX1 <= 1 && mX2 >= 0 && mX2 <= 1)) {
       throw new Error('bezier x values must be in [0, 1] range');
@@ -169,7 +166,7 @@ function AnimGroup(inputKeyframes = []) {
       sampleValues[i] = calcBezier(i * kSampleStepSize, mX1, mX2);
     }
 
-    const getTForX = aX => {
+    const getTForX = (aX) => {
       let intervalStart = 0.0;
       let currentSample = 1;
       const lastSample = kSplineTableSize - 1;
@@ -184,9 +181,8 @@ function AnimGroup(inputKeyframes = []) {
       --currentSample;
 
       // Interpolate to provide an initial guess for t
-      const dist =
-        (aX - sampleValues[currentSample]) /
-        (sampleValues[currentSample + 1] - sampleValues[currentSample]);
+      const dist = (aX - sampleValues[currentSample])
+        / (sampleValues[currentSample + 1] - sampleValues[currentSample]);
       const guessForT = intervalStart + dist * kSampleStepSize;
       const initialSlope = getSlope(guessForT, mX1, mX2);
 
@@ -201,11 +197,11 @@ function AnimGroup(inputKeyframes = []) {
         intervalStart,
         intervalStart + kSampleStepSize,
         mX1,
-        mX2
+        mX2,
       );
     };
 
-    const bezierEasing = x => {
+    const bezierEasing = (x) => {
       if (x === 0) {
         return 0;
       }
@@ -219,8 +215,8 @@ function AnimGroup(inputKeyframes = []) {
   };
 
   // Returns the final animated value
-  const animateBetweenKeys = function(
-    time = requiredArgumentError('time', '.anim inputs')
+  const animateBetweenKeys = function (
+    time = requiredArgumentError('time', '.anim inputs'),
   ) {
     checkTypes([[time, 'number']]);
     const lastKeyNum = keys.length - 1;
@@ -255,7 +251,7 @@ function AnimGroup(inputKeyframes = []) {
       curKey.easeOut / 100,
       curKey.velocityOut / 100,
       1 - nextKey.easeIn / 100,
-      1 - nextKey.velocityIn / 100
+      1 - nextKey.velocityIn / 100,
     );
 
     // Delta calculations
@@ -276,7 +272,7 @@ function AnimGroup(inputKeyframes = []) {
     const animateArrayFromProgress = (startArray, endArray, progressAmount) => {
       // Array Subtraction
       const arrayDelta = endArray.map(
-        (item, index) => item - startArray[index]
+        (item, index) => item - startArray[index],
       );
       // Multiply difference by progress
       const deltaProgressed = arrayDelta.map(item => item * progressAmount);
