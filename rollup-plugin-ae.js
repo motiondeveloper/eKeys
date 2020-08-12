26136d2937d1a2956f34ed138f9f9fa9f56efd84
timhaywood <tim@haywood.org>
2020-08-12 12:41:45 +1000

    seperate exports with commas

--- rollup-plugin-ae.js
+++ rollup-plugin-ae.js
@@ -5,55 +5,56 @@ export default function afterEffectsJsx(options = {}) {
       // format each file
       // to be ae-jsx
       for (const file in bundle) {
-        console.log(file);
         const originalFile = bundle[file];
         const originalCode = originalFile.code;
-        const lines = originalCode.split('\n');
-        const exportLines = lines.filter(line => line.startsWith('exports.'));
-        const exportNames = exportLines.map(name =>
-          name.replace(/exports\..+\s=\s/g, '').replace(';', '')
-        );
+        const exportNames = originalCode
+          .split('\n')
+          .filter(line => line.startsWith('exports.'))
+          .map(name => name.replace(/exports\..+\s=\s/g, '').replace(';', ''));
 
+        // Remove code rollup adds
         let fixedCode = originalCode
-          // Remove use strict
           .replace("'use strict';", '')
           .replace(
             "Object.defineProperty(exports, '__esModule', { value: true });",
             ''
           )
-          // Remove the module.exports
-          // whether object or single variable
           .replace(/exports\..+\s=\s(({([^;]*)})|.+);/g, '');
-        // Replace exported values
-        // with jsx compliant syntax
-        exportNames.forEach(
-          name =>
-            (fixedCode = fixedCode
-              .replace(`function ${name}`, `"${name}": function`)
-              .replace(`const ${name} =`, `"${name}": `)
-              .replace(`let ${name} =`, `"${name}": `)
-              .replace(`var ${name} =`, `"${name}": `)
-              // Separate functions with commas
-              .replace(`}\n"${name}"`, `},\n"${name}"`)
-              // Separate values with commas
-              .replace(`;\n"${name}"`, `,\n"${name}"`))
-        );
+
+        // Replace exported value definitions
+        // with jsx compliant (json) syntax
+        exportNames.forEach(name => {
+          fixedCode = fixedCode
+            .replace(`function ${name}`, `"${name}": function`)
+            .replace(`const ${name} =`, `"${name}": `)
+            .replace(`let ${name} =`, `"${name}": `)
+            .replace(`var ${name} =`, `"${name}": `)
+            .replace(`}\n"${name}"`, `}\n"${name}"`)
+            .replace(`;\n"${name}"`, `,\n"${name}"`);
+        });
+
+        // Separate exported items
+        // with commas not semi-colons
+        let codeLines = fixedCode.split('\n').map(line => {
+          let newLine = line;
+          exportNames.forEach(name => {
+            if (line.startsWith(`"${name}"`)) {
+              newLine = newLine.replace(';', ',');
+            }
+          });
+          if (newLine === '}') {
+            newLine = '},';
+          }
+          return newLine;
+        });
 
         // Indent code
-        fixedCode = fixedCode
-          .split('\n')
-          .map(line => `	${line}`)
-          .join('\n');
-
+        fixedCode = codeLines.map(line => `	${line}`).join('\n');
         // Wrap in braces
         const newCode = `{\n	${fixedCode.trim()}\n}`;
-        // Delete original file
-        delete bundle[file];
-        // Add file with replaced code
-        bundle[file] = {
-          ...originalFile,
-          code: newCode,
-        };
+
+        // Add replace code of file with modified
+        bundle[file].code = newCode;
       }
     },
   };
